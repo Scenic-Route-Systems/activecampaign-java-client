13d93c3c124d2581b61b26487ff6f058928b7f7d
Brett Meyer <brett@scenicroute.systems>
2025-04-25 22:38:10 -0400

    stage list request

--- src/main/java/org/sourcelab/activecampaign/apiv3/ActiveCampaignClient.java
+++ src/main/java/org/sourcelab/activecampaign/apiv3/ActiveCampaignClient.java
@@ -37,6 +37,7 @@ import org.sourcelab.activecampaign.apiv3.request.deal.DealCustomFieldListReques
 import org.sourcelab.activecampaign.apiv3.request.deal.DealSearch;
 import org.sourcelab.activecampaign.apiv3.request.deal.DealSearchRequest;
 import org.sourcelab.activecampaign.apiv3.request.list.ListListRequest;
+import org.sourcelab.activecampaign.apiv3.request.stage.StageListRequest;
 import org.sourcelab.activecampaign.apiv3.request.tag.TagCreateRequest;
 import org.sourcelab.activecampaign.apiv3.request.tag.TagListRequest;
 import org.sourcelab.activecampaign.apiv3.request.user.UsersMeRequest;
@@ -52,6 +53,7 @@ import org.sourcelab.activecampaign.apiv3.response.customField.CustomFieldListRe
 import org.sourcelab.activecampaign.apiv3.response.deal.DealCustomFieldListResponse;
 import org.sourcelab.activecampaign.apiv3.response.deal.DealSearchResponse;
 import org.sourcelab.activecampaign.apiv3.response.list.ListListResponse;
+import org.sourcelab.activecampaign.apiv3.response.stage.StageListResponse;
 import org.sourcelab.activecampaign.apiv3.response.tag.TagCreateResponse;
 import org.sourcelab.activecampaign.apiv3.response.tag.TagListResponse;
 import org.sourcelab.activecampaign.apiv3.response.user.UsersMeResponse;
@@ -187,6 +189,10 @@ public class ActiveCampaignClient extends AbstractClient {
      */
     public CustomFieldListResponse customFieldList() {
         return submitRequest(new CustomFieldListRequest());
+    }
+
+    public StageListResponse stageList() {
+        return submitRequest(new StageListRequest());
     }
 
     public ListListResponse listList() {
