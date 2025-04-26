c005d99bdca165de514d14271fd7ab2c4e27b1b9
Brett Meyer <brett@scenicroute.systems>
2025-04-26 00:18:43 -0400

    DealSearch.withStage

--- src/main/java/org/sourcelab/activecampaign/apiv3/request/deal/DealSearch.java
+++ src/main/java/org/sourcelab/activecampaign/apiv3/request/deal/DealSearch.java
@@ -4,14 +4,19 @@ import org.sourcelab.activecampaign.apiv3.request.DTOWithQueryString;
 
 public class DealSearch extends DTOWithQueryString {
 
-  public DealSearch withContact(final String contact) {
-    queryParams.put("filters[contact]", contact);
+  public DealSearch withContact(final long contactId) {
+    queryParams.put("filters[contact]", contactId + "");
     return this;
   }
 
   // yyyy-mm-dd
   public DealSearch withCreatedAfter(final String after) {
     queryParams.put("filters[created_after]", after);
+    return this;
+  }
+
+  public DealSearch withStage(final long stageId) {
+    queryParams.put("filters[stage]", stageId + "");
     return this;
   }
 }
